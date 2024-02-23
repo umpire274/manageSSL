78518494e86df37af302e0577b48e7f4a8c93064
Alessandro Maestri <a.maestri@almaviva.it>
2024-02-23 11:57:03 +0100

    Changed teh default end-of-line of the sources from CRLF to LF

--- standalone/register-crt.py
+++ standalone/register-crt.py
@@ -3,7 +3,7 @@ import os
 
 # check ca-cert/ca.crt and ca-cert/ca.key
 if os.path.exists("ca-cert/ca.crt") and os.path.exists("ca-cert/ca.key"):
-    print ("CA is ready")
+    print("CA is ready")
 else:
     print("CA is not ready, generate it first")
     print("Run generate-ca.py")
@@ -15,7 +15,7 @@ csrFile = input("Enter the CSR file name: ")
 # print csr information
 print("CSR information:")
 # csr decoder and return the subject
-csrInfo = sp.check_output(['openssl', 'req', '-in', csrFile, '-noout','-subject'])
+csrInfo = sp.check_output(['openssl', 'req', '-in', csrFile, '-noout', '-subject'])
 print("".join(csrInfo.decode('utf-8').split("subject=")[1].replace(", ", "\n")))
 # validate csr
 print("Do you want to continue? (y/n)")
@@ -24,11 +24,12 @@ if input().lower() == "y":
 else:
     exit()
 
-
 # input expiration days
 validDate = input("Enter the expiration (days) of the certificate: ")
 
-crtFile = os.path.splitext(csrFile)[0]+'.crt'
+crtFile = os.path.splitext(csrFile)[0] + '.crt'
 
 # openssl x509 -req -in fabrikam.csr -CA  contoso.crt -CAkey contoso.key -CAcreateserial -out fabrikam.crt -days 365 -sha256
-sp.call(['openssl', 'x509', '-req', '-in', csrFile, '-CA', 'ca-cert/ca.crt', '-CAkey', 'ca-cert/ca.key', '-CAcreateserial', '-out', crtFile, '-days', validDate, '-sha256'])
+sp.call(
+    ['openssl', 'x509', '-req', '-in', csrFile, '-CA', 'ca-cert/ca.crt', '-CAkey', 'ca-cert/ca.key', '-CAcreateserial',
+     '-out', crtFile, '-days', validDate, '-sha256'])
