863dd9429138fd50ae0bbca331e43e52eb4e3ea9
Alessandro Maestri <a.maestri@almaviva.it>
2024-02-27 11:25:39 +0100

    Changed entire source from module 'pyOpenSSL' to 'cryptography'

--- Classes/CertificateAuthority.py
+++ Classes/CertificateAuthority.py
@@ -1,12 +1,12 @@
-import shutil
+import datetime
 import os
+import shutil
 
-from OpenSSL import crypto
-
-# Variables
-TYPE_RSA = crypto.TYPE_RSA
-TYPE_DSA = crypto.TYPE_DSA
-FILETYPE_PEM = crypto.FILETYPE_PEM
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.x509.oid import NameOID
 
 
 class CertificateAuthority:
@@ -66,33 +66,47 @@ CN = """ + commonname + """
 
     def generate_root_key(self, bites=2048):
         self.file_ca_key = os.path.join(self.dir_ca, 'ca.key')
-        ca_key = crypto.PKey()
-        ca_key.generate_key(TYPE_RSA, bites)
+        ca_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=bites,
+            backend=default_backend()
+        )
         # Save private key
         with open(self.file_ca_key, "wt") as f:
-            f.write(crypto.dump_privatekey(FILETYPE_PEM, ca_key).decode('utf-8'))
+            f.write(ca_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption(),
+            ).decode('utf-8'))
 
     def create_csr(self):
         self.file_ca_csr = os.path.join(self.dir_ca, 'ca.csr')
-        req = crypto.X509Req()
         with open(self.file_ca_config, "r") as f:
             lines = f.readlines()
             for line in lines:
                 if line.startswith("C = "):
-                    req.get_subject().C = line.split("=")[1].strip()
+                    country_name = line.split("=")[1].strip()
                 elif line.startswith("O = "):
-                    req.get_subject().O = line.split("=")[1].strip()
+                    org_name = line.split("=")[1].strip()
                 elif line.startswith("OU = "):
-                    req.get_subject().OU = line.split("=")[1].strip()
+                    org_unit_name = line.split("=")[1].strip()
                 elif line.startswith("CN = "):
-                    req.get_subject().CN = line.split("=")[1].strip()
+                    common_name = line.split("=")[1].strip()
         with open(self.file_ca_key, "rt") as f:
             key_data = f.read()
-        private_key = crypto.load_privatekey(FILETYPE_PEM, key_data)
-        req.set_pubkey(private_key)
-        req.sign(private_key, "sha256")
+        private_key = serialization.load_pem_private_key(
+            key_data.encode(),
+            password=None,
+            backend=default_backend()
+        )
+        csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
+            x509.NameAttribute(NameOID.COUNTRY_NAME, country_name),
+            x509.NameAttribute(NameOID.ORGANIZATION_NAME, org_name),
+            x509.NameAttribute(NameOID.ORGANIZATIONAL_UNIT_NAME, org_unit_name),
+            x509.NameAttribute(NameOID.COMMON_NAME, common_name),
+        ])).sign(private_key, hashes.SHA256(), default_backend())
         with open(self.file_ca_csr, "wt") as f:
-            f.write(crypto.dump_certificate_request(FILETYPE_PEM, req).decode('utf-8'))
+            f.write(csr.public_bytes(serialization.Encoding.PEM).decode('utf-8'))
 
     def create_self_signed_certificate(self, valid_days):
         # Inizializza self.file_ca_cert con un percorso di file valido
@@ -100,22 +114,35 @@ CN = """ + commonname + """
 
         # Carica la chiave privata
         with open(self.file_ca_key, "rt") as key_file:
-            private_key = crypto.load_privatekey(FILETYPE_PEM, key_file.read().encode())
+            private_key = serialization.load_pem_private_key(
+                key_file.read().encode(),
+                password=None,
+                backend=default_backend()
+            )
 
         # Carica la richiesta CSR
         with open(self.file_ca_csr, "rt") as csr_file:
-            csr = crypto.load_certificate_request(FILETYPE_PEM, csr_file.read().encode())
+            csr = x509.load_pem_x509_csr(
+                csr_file.read().encode(),
+                default_backend()
+            )
 
         # Crea un certificato autofirmato
-        cert = crypto.X509()
-        cert.set_subject(csr.get_subject())
-        cert.set_pubkey(csr.get_pubkey())
-        cert.gmtime_adj_notBefore(0)
-        cert.gmtime_adj_notAfter(valid_days * 24 * 60 * 60)
-        cert.set_issuer(cert.get_subject())
-        cert.set_serial_number(1000)
-        cert.sign(private_key, 'sha256')
+        cert = (
+            x509.CertificateBuilder()
+            .subject_name(csr.subject)
+            .issuer_name(csr.subject)
+            .public_key(private_key.public_key())
+            .serial_number(x509.random_serial_number())
+            .not_valid_before(datetime.datetime.now(datetime.UTC))
+            .not_valid_after(datetime.datetime.now(datetime.UTC) + datetime.timedelta(days=valid_days))
+            .add_extension(
+                x509.SubjectAlternativeName([x509.DNSName(u"localhost")]),
+                critical=False,
+            )
+            .sign(private_key, hashes.SHA256(), default_backend())
+        )
 
         # Salva il certificato
         with open(self.file_ca_cert, "wt") as f:
-            f.write(crypto.dump_certificate(FILETYPE_PEM, cert).decode('utf-8'))
+            f.write(cert.public_bytes(serialization.Encoding.PEM).decode('utf-8'))
